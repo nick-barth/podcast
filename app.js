915844fdc6c2a8d909f2665d616252c70b25c585
Nick Barth <barth.nicholas@gmail.com>
2017-02-25 14:28:44 +0100

    lots of good stuff

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var path = require('path');
 
 var app = express();
 
@@ -20,7 +21,42 @@ var job = new CronJob('00 30 11 * * 1-5', function() {
 const episodes = [{
   "id": 13158665,
   "created_at": "2011/04/06 15:37:43 +0000",
-  "user_id": "3699101",
+  "user_id": "2",
+  "duration": 18109,
+  "commentable": true,
+  "state": "finished",
+  "sharing": "public",
+  "permalink": "munching-at-tiannas-house",
+  "description": "John Doe, expert at esports spits some knowledge!",
+  "streamable": true,
+  "downloadable": true,
+  "genre": null,
+  "release": null,
+  "purchase_url": null,
+  "label_id": null,
+  "label_name": null,
+  "isrc": null,
+  "video_url": null,
+  "track_type": "recording",
+  "title": "1: Competative Cooking",
+  "original_format": "m4a",
+  "original_content_size": 10211857,
+  "license": "all-rights-reserved",
+  "uri": "http://api.soundcloud.com/tracks/13158665",
+  "permalink_url": "http://soundcloud.com/user2835985/munching-at-tiannas-house",
+  "artwork_url": null,
+  "waveform_url": "http://w1.sndcdn.com/fxguEjG4ax6B_m.png",
+  "download_url": "http://api.soundcloud.com/tracks/13158665/download",
+  "playback_count": 0,
+  "download_count": 0,
+  "favoritings_count": 0,
+  "comment_count": 0,
+  "attachments_uri": "http://api.soundcloud.com/tracks/13158665/attachments"
+},
+{
+  "id": 1,
+  "created_at": "2011/04/06 15:37:43 +0000",
+  "user_id": "1",
   "duration": 18109,
   "commentable": true,
   "state": "finished",
@@ -58,25 +94,21 @@ var hbs = exphbs.create({
     partialsDir: 'views/partials/'
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
 	res.render('layouts/main', {
-		title: 'Wow',
-
-		// This `message` will be transformed by our `yell()` helper.
-		message: 'hello world'
+		episodes: episodes,
 	});
 });
 
 episodes.forEach(function(pageObj){
-	app.get('/' + pageObj.user_id, function (req, res) {
+	app.get('/episode/' + pageObj.user_id, function (req, res) {
 	    res.render('layouts/episode', {
-	        title: 'Wow',
-
-	        // This `message` will be transformed by our `yell()` helper.
-	        message: 'hello world'
+			episode: pageObj
     	});
 	});
 });
